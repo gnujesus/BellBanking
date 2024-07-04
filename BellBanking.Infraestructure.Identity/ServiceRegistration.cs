fa3a6f6e8c5423d4646874d7b043ef61960470b8
Gabriel Ascencio <gascenciov.gav@gmail.com>
2024-07-04 06:08:36 -0400

    Identity Layer Completed With User Service And AutoMapper

---
+++ BellBanking.Infraestructure.Identity/ServiceRegistration.cs
@@ -0,0 +1,51 @@
+﻿using BellBanking.Core.Application.Interfaces.Services;
+using BellBanking.Infraestructure.Identity.Contexts;
+using BellBanking.Infraestructure.Identity.Entities;
+using BellBanking.Infraestructure.Identity.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BellBanking.Infraestructure.Identity
+{
+    public static class ServiceRegistration
+    {
+        public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Contexts
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<IdentityContext>(options => options.UseInMemoryDatabase("IdentityDb"));
+            }
+            else
+            {
+                services.AddDbContext<IdentityContext>(options =>
+                {
+                    options.EnableSensitiveDataLogging();
+                    options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"),
+                    m => m.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName));
+                });
+            }
+            #endregion
+
+            #region Identity    
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/User";
+                options.AccessDeniedPath = "/User/AccessDenied";
+            });
+
+            services.AddAuthentication();
+            #endregion
+
+            #region Services
+            services.AddTransient<IAccountService, AccountService>();
+            #endregion
+        }
+    }
+}
